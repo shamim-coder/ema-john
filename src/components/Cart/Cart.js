c4e7c9c12faecdc936e08bb4cf5433339ec6504b
Shamim Reza <wpshamim@gmail.com>
2021-02-28 00:45:03 +0600

    ema john almost done

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,11 +1,58 @@
 import React from 'react';
+import "./Cart.css";
 
-const Cart = () => {
+const Cart = (props) => {
+    const cartLength = props.cart.length
+    const cart = props.cart
+    const totalPrice = cart.reduce((total, current) => total + current.price, 0);
+    let totalShipping = 0;
+    if (totalPrice > 900) {
+        totalShipping = 0
+    } else {
+        totalShipping = cart.reduce((total, current) => total + current.shipping, 0);
+    }
+    const totalWithoutTax = totalPrice + totalShipping
+    const tax = (0.10 * totalWithoutTax)
+    const grandTotal = tax + totalWithoutTax
+
+    const formatNumber = (number) => {
+        const decimalNumber = number.toFixed(2)
+        return Number(decimalNumber)
+    }
+
     return (
-        <div>
-            <h1>this is the cart</h1>
-        </div>
+        <>
+            <h1>Order Summary</h1>
+            <p>item ordered : {cartLength}</p>
+
+            <table>
+                <tbody>
+                    <tr>
+                        <td>Items:</td>
+                        <td>${formatNumber(totalPrice)}</td>
+                    </tr>
+                    <tr>
+                        <td>Shipping & Handling:</td>
+                        <td>{formatNumber(totalShipping)}</td>
+                    </tr>
+                    <tr>
+                        <td>Total before tax:</td>
+                        <td>{formatNumber(totalWithoutTax)}</td>
+                    </tr>
+                    <tr>
+                        <td>Estimated Tax:</td>
+                        <td>{formatNumber(tax)}</td>
+                    </tr>
+                    <tr className="grand-total">
+                        <td>Order Total:</td>
+                        <td>{formatNumber(grandTotal)}</td>
+                    </tr>
+                </tbody>
+            </table>
+
+        </>
     );
 };
 
 export default Cart;
+
--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -3,18 +3,25 @@ import fakeData from '../../resources/fakeData';
 import { useState } from "react";
 import Product from '../Product/Product';
 import Cart from '../Cart/Cart';
-import { } from "./Shop.css";
+import "./Shop.css";
 
 const Shop = () => {
     const firstTenData = fakeData.slice(0, 10)
     const [products, setProducts] = useState(firstTenData)
+    const [cart, setCart] = useState([])
+    const cartHandler = (product) => {
+        const newCart = [...cart, product]
+        setCart(newCart)
+    }
     return (
         <div className="shop-container">
             <div className="products-container">
-                <Product items={products} key={products.key} />
+                {
+                    products.map(product => <Product product={product} cartHandler={cartHandler} key={product.key} />)
+                }
             </div>
             <aside className="cart-container">
-                <Cart />
+                <Cart cart={cart} />
             </aside>
         </div>
     );
